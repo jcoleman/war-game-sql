58c3000e9b2a0c7724cfecbf993e992838a984e0
James Coleman <jtc331@gmail.com>
2015-09-29 17:02:06 -0400

    Add support for repeated face values (the "war" case.)

--- game.sql
+++ game.sql
@@ -1,8 +1,8 @@
 with recursive
   deck_cards as (
     select face_values.card, suits.i as suit
-    from generate_series(1, 6) as face_values(card)
-    cross join generate_series(1, 1) as suits(i)
+    from generate_series(1, 13) as face_values(card)
+    cross join generate_series(1, 4) as suits(i)
     order by random()
   ),
   deck_cards_with_player_indices as (
@@ -14,11 +14,12 @@ with recursive
     from deck_cards_with_player_indices
     group by player_index
   ),
-  simulation_hands (hand, packet_1, packet_2, winner) as (
+  simulation_hands (hand, packet_1, packet_2, pending_cards, winner) as (
     select
       1 as hand,
       (select packet from player_packets where player_index = 0) as packet_1,
       (select packet from player_packets where player_index = 1) as packet_2,
+      array[]::integer[],
       null::integer
 
     union all
@@ -26,19 +27,20 @@ with recursive
     (
       select
         hand + 1 as hand,
-        (case simulation_hand_calculations.winner when 1 then pending_cards || retained_packet_1 else retained_packet_1 end) as packet_1,
-        (case simulation_hand_calculations.winner when 2 then pending_cards || retained_packet_2 else retained_packet_2 end) as packet_2,
+        (case simulation_hand_calculations.winner when 1 then simulation_hand_calculations.pending_cards || retained_packet_1 else retained_packet_1 end) as packet_1,
+        (case simulation_hand_calculations.winner when 2 then simulation_hand_calculations.pending_cards || retained_packet_2 else retained_packet_2 end) as packet_2,
+        (case when simulation_hand_calculations.winner is null then simulation_hand_calculations.pending_cards else array[]::integer[] end) as pending_cards,
         simulation_hand_calculations.winner
       from (
         select *
         from simulation_hands
         order by hand desc
         limit 1
-      ) last_simulation_hand_packets
+      ) last_simulation_results
       inner join lateral (
         select
-          last_simulation_hand_packets.packet_1[1:(array_length(packet_1, 1) - 1)] as retained_packet_1,
-          last_simulation_hand_packets.packet_2[1:(array_length(packet_2, 1) - 1)] as retained_packet_2,
+          last_simulation_results.packet_1[1:(array_length(packet_1, 1) - 1)] as retained_packet_1,
+          last_simulation_results.packet_2[1:(array_length(packet_2, 1) - 1)] as retained_packet_2,
           array[
             coalesce(packet_1[array_length(packet_1, 1)], -1),
             coalesce(packet_2[array_length(packet_2, 1)], -1)
@@ -48,12 +50,12 @@ with recursive
         select
           (
             select array_agg(c.n order by random())
-            from unnest(last_cards) c(n)
+            from unnest(last_cards || last_simulation_results.pending_cards) c(n)
             where c.n > -1
           ) as pending_cards,
           (
             case
-            when last_cards[1] = last_cards[2] then null
+            when last_cards[1] = last_cards[2] then null::integer
             when last_cards[1] > last_cards[2] then 1
             else 2
             end
@@ -64,7 +66,7 @@ with recursive
           packet_1 = packet_2 -- stalemate
           or array_length(packet_1, 1) = 0
           or array_length(packet_2, 1) = 0
-          or hand > 10
+          or hand > 10000 -- safety kill switch for development; remove in final version
         )
     )
   )
